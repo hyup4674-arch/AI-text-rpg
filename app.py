321338641f20ce34137258f0783e53c4ce25d4ac
hyup4674-arch <hyup4674@gmail.com>
2026-08-26 01:14:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -131,11 +131,6 @@ st.markdown(
 # 모델 목록 설정
 default_models = [
     "gemini-3.1-flash-lite",
-    "gemini-3.1-flash",
-    "gemini-3.6-flash",
-    "gemini-3.7-flash",
-    "gemini-3.5-flash-lite",
-    "gemini-3.5-flash",
 ]
 
 available_models = default_models
